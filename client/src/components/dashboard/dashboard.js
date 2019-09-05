5f1fe4a474c5550deabdc6ce864059a00e91f2a1
MirceaNegoita <mircea.negoita13@gmail.com>
2019-09-06 00:37:26 +0300

    Added dashboard section

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,17 +1,109 @@
+// External imports
 import React, {Component} from 'react';
-import { Link, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 
+// Material UI imports
+import { Grid, Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core';
 
-const styles = {
+// Internal imports
+import ApiHelper from '../../helpers/api';
+import DashboardWidget from '../../core/dashboard-widget';
+import { ErrorOutline } from '@material-ui/icons';
 
+const styles = {
+    errorMessage: {
+        color: "rgba(0, 0, 0, 0.54)",
+        fontSize: "48px",
+        textAlign: "center"
+    }
 };
 
 class Dashboard extends Component{
-    render(){
+    constructor(props){
+        super(props);
+        this.state = {
+            sets: [],
+            ocrs: [],
+        };
+    }
+
+    loadSets(){
+        ApiHelper.get("sets")
+        .then(response => {
+            if (response.data) {
+                this.setState({ sets: response.data });
+            }
+        })
+        .catch(error => {
+            throw error;
+        });
+    }
+
+    loadOcrs(){
+        ApiHelper.get("ocr")
+        .then(response => {
+            if (response.data) {
+                this.setState({ ocrs: response.data });
+            }
+        })
+        .catch(error => {
+            throw error;
+        });
+    }
+
+    componentDidMount(){
+        this.loadSets();
+        this.loadOcrs();
+    }
+
+    get formatedData(){
+        const { sets, ocrs } = this.state;
+        let elements = [];
+
+        if (sets.length > 0) {
+            elements.push({ header: "File Comparisons", section: "sets", data: sets });
+        }
+
+        if (ocrs.length > 0) {
+            elements.push({ header: "OCR Results", section: "ocr", data: ocrs });
+        }
+
+        return elements;
+    }
+
+    get noWidgetsMessage(){
+        const { classes } = this.props;
+
+        return <Typography component="h1" className={classes.errorMessage}>No widgets to display</Typography>;
+    }
+
+    get widgets(){
+        const elements = this.formatedData;
+
         return (
-            <p>Dashboard view</p>
+            <Grid container spacing={3}>
+                {elements.map(element => (
+                    <Grid key={element.section} xs={12 / elements.length} item={true}>
+                        <DashboardWidget
+                            key={element.section}
+                            header={element.header}
+                            section={element.section}
+                            data={element.data} />
+                    </Grid>
+                ))}
+            </Grid>
         );
+    }
+
+    render(){
+        const elements = this.formatedData;
+
+        if (elements.length === 0) {
+            return this.noWidgetsMessage
+        }
+
+        return this.widgets;
     }
 }
 
